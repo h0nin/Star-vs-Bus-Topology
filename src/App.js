cb72c98c3f8dd4388868d0369563f9bf4256ccaf
h0nin <anonymouse5637@proton.me>
2025-03-22 07:23:22 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import YearImageViewer from "./components/YearImageViewer";
+
+function App() {
+  return (
+    <div className="App">
+      <YearImageViewer />
+    </div>
+  );
+}
+
+export default App;
